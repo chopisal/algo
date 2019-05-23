92150a7097371d8df54ddf14e9029ef3b2305802
SeongBongKim <chopisal@gmail.com>
2019-05-23 15:05:58 +0900

    budget

--- budget/solution.py
+++ budget/solution.py
@@ -6,39 +6,61 @@ def solution(budgets, M):
     sum = 0
     for val in sorted_budgets:
         sum += val
-    print("sum =>", sum, M)
+    print("request budgets sum =>", sum, "  budget:", M)
     if M > sum:
         return sorted_budgets[len(budgets) - 1]
-    # print(sorted(budgets))
+    print(sorted(budgets))
 
     average = M / len(budgets)
     pos = bisect.bisect(sorted_budgets, average)
     print("average", average, "  pos", pos)
     print(sorted_budgets)
     if pos == 0:
-        return average
+        return int(average)
 
     remain = 0
     for budget in sorted_budgets[0:pos]:
         remain += average - budget
 
-    up_remain = 0
-    for budget in sorted_budgets[pos:]:
-        up_remain += budget - average
+    plus_budget = 0
+    over_budgets = sorted_budgets[pos:]
 
-    sum_bud = int(remain / (len(budgets) - pos))
+    for index, budget in enumerate(over_budgets):
+        need_budget = (budget - average) * (len(over_budgets) - index)
 
-    print("average =", remain, "  up_remain =", up_remain)
-    print("--- ", up_remain - remain)
-    add_bug = up_remain - remain
+        if remain > need_budget:
+            remain = remain - need_budget
+            plus_budget = budget - average
+            average += plus_budget
+            # print("over budget ", budget,  "  need_budget=",
+            #       need_budget, "  remain=", remain, "   average=", average)
+        else:
+            plus_budget = remain / (len(over_budgets) - index)
+            average += plus_budget
+            # print("2over budget ", budget,  "  need_budget=",
+            #       need_budget, "  remain=", remain, "   average=", average)
+            break
 
-    for i in range(len(budgets)-1, pos-1, -1):
-        diff = sorted_budgets[i] - sorted_budgets[i-1]
-        add_bug = add_bug - diff
-        print()
+    # print("average =", average, "  plus_budget =", plus_budget)
+    # print("answer=", int(plus_budget + average))
+
     print("end")
-    return sum_bud + average
+    return int(average)
 
 
-ans = solution([120, 110, 140, 150, 133], 641)
+def solution2(budgets, M):
+    lim = max(budgets)
+
+    while True:
+        if sum(budgets) < M:
+            answer = max(budgets)
+            break
+
+        budgets = list(map(lambda x: x if x <= lim else lim, budgets))
+        lim = lim-1
+
+    return answer
+
+
+ans = solution([120, 110, 140, 150, 133], 639)
 print(ans)
